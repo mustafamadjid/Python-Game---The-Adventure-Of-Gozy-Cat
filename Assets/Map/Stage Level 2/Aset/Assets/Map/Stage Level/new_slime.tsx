2373772a3637cb5113c69fe615e87213d7082d34
zeitfaith <hanbalifatih@gmail.com>
2024-05-17 18:11:13 +0700

    Update Map Stage Level

---
+++ Assets/Map/Stage Level 2/Aset/Assets/Map/Stage Level/new_slime.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="new_slime" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../Enemy/slime_2/1.png" width="66" height="74"/>
+</tileset>
---
+++ Assets/Map/Stage Level 2/TSX/Trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Trees" tilewidth="64" tileheight="64" tilecount="2" columns="2">
+ <image source="../Aset/Background Layers 2/Trees.png" width="128" height="96"/>
+</tileset>
