e7969ce498afeaed60d792e234f7e72e1339d9db
unknown <eden.122140187@student.itera.ac.id>
2024-05-18 15:45:30 +0700

    update slime_2

--- Assets/Map/revisi fatih lagi/File TILED/File TILED/Map and Stage/Forest Props.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Forest Props" tilewidth="64" tileheight="64" tilecount="63" columns="7">
- <image source="../forest tileset/Forest Props.png" width="480" height="592"/>
-</tileset>
